d4a216a697271737a4b3d5858889cdd1a6b8164c
Anwesh43 <anweshthecool0@gmail.com>
2021-02-10 00:23:18 +0530

    adding constants and libraries

--- bidropbarballview/src/main/java/com/example/bidropbarballview/BiDropBallBarView.kt
+++ bidropbarballview/src/main/java/com/example/bidropbarballview/BiDropBallBarView.kt
@@ -1,2 +1,24 @@
 package com.example.bidropbarballview
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+    "#F44336",
+    "#2196F3",
+    "#8BC34A",
+    "#3F51B5",
+    "#009688"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val backColor : Int = Color.parseColor("#BDBDBD")
+val barHFactor : Float = 11.2f
+val ballRFactor : Float = 5.9f
+val delay : Long = 20
